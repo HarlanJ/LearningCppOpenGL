dcdb0e961e189483cdb263400e36165d07b14ecb
John Gerardo-Harlan <sparkyjohn.harlan@gmail.com>
2024-10-14 22:02:13 -0400

    Adjustment to frag shader for the fading animation I actually wanted

--- assets/shaders/frag.glsl
+++ assets/shaders/frag.glsl
@@ -8,6 +8,8 @@ uniform float time;
 
 out vec4 FragColor;
 
+const float pi = 3.1415926535;
+
 void main(){
-    FragColor = texture(tex1, uv)*max(0,sin(time)) + texture(tex2, uv)*max(0,sin(time)*-1);
+    FragColor = (texture(tex1, uv)*(sin(time)+1)/2.0f) + (texture(tex2, uv)*(sin(time+pi)+1)/2.0f);
 }
